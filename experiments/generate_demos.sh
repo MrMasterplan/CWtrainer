c822c050080b0c699574ff44eeda6928f3cef8c0
MrMasterplan <simon@heisterkamp.dk>
2018-03-09 23:11:32 +0100

    just about ready now

--- experiments/generate_demos.sh
+++ experiments/generate_demos.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 
 #This script generates the complete audio files from the demos.
@@ -7,7 +7,9 @@
 #../lib/ScriptGenerator.py -t demo_control.template -o demo_template_result.script
 
 #This is how a script is converted to audio
-#../lib/ScriptReader.py -o demo_manual.wav demo_manual.script
+../lib/ScriptReader.py -o demo_manual.mp3 demo_manual.script
+#the ending of the audio file can be anyting that ffmpeg recognizes.
+# this format can also be given in the -f option
 
 #The intermediate script does not have to be saved is used like this:
 ../lib/ScriptGenerator.py -t demo_control.template -o - | ../lib/ScriptReader.py -o demo_control.mp3 -  
